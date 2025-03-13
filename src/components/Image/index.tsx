404d6014ee9e26592c9676bdc674b890da6b31f9
lidiasanchez <blue98.bee.dev@gmail.com>
2025-03-13 01:15:28 +0100

    feature: added button close to remove image. TODO feature

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,8 +1,15 @@
+import styles from './index.module.css';
+import { IoMdCloseCircle } from "react-icons/io";
+
+
 type ImageProps = {
     name: string,
     image: string
 }
 
 export const Image = ({ name, image}: ImageProps) => {
-    return <img alt={name} src={image} />
+    return <div className={styles.wrapper}>
+        <button className={styles["button-close"]}> <IoMdCloseCircle /> </button>
+        <img alt={name} src={image} className={styles.image} />
+    </div>
 }
